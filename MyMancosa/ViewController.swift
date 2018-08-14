2e9be7b08a347e18d52d5a0c12e10fba31a846eb
Nerudo Mregi <nerudo@macbooks-macbook-pro.local>
2018-08-14 08:55:44 +0200

    Initial Commit

---
+++ MyMancosa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMancosa
+//
+//  Created by Nerudo Mregi on 2018/08/14.
+//  Copyright © 2018 Nerudo Mregi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
